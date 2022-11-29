1f34018372c0fb444781cdd562a43439ed9da4ec
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-29 04:27:40 +0100

    app structure ready

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import home from './Home.module.css'
+function Home() {
+  return (
+    <div className={home.homeWrapper}>
+        Home
+    </div>
+  )
+}
+
+export default Home
