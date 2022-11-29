a824fe52005420bef9d1236df7bc52c8a40d0c63
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-29 09:29:41 +0100

    feat and styling: navbar lists icon addded with their styling

--- src/components/layouts/Layouts.js
+++ src/components/layouts/Layouts.js
@@ -1,24 +1,33 @@
 import { Link } from "react-router-dom";
 import githubLogo from '../../assets/githubLogo.png'
 import layout from "./Layout.module.css";
-
+import homeIcon from '../layouts/assets/home.png';
+import folowersIcon from '../layouts/assets/followers.png';
+import followingIcon from '../layouts/assets/following.png';
+import repoIcon from '../layouts/assets/repo.png';
+import watchIcon from '../layouts/assets/watch.png';
 //Navbar
 export const Navbar = () => {
   return (
-    <div className={layout.layoutWrapper}>
+    <div className={layout.navbarWrapper}>
       <ul className={layout.navbarListFlex}>
         <div className={layout.logoDiv}>
-          <img src={githubLogo} alt="logo" className={layout.appLogo} />
+        <img src={githubLogo} alt="logo" className={layout.appLogo} />
           <h2 className={layout.appLogoText}>Analazer App</h2>
         </div>
-        <div className={layout.listsDiv}>
-          <li className={layout.list}>
-            <Link to="/">Home</Link>
+        <div className={layout.navbarListsDiv}>
+          <li className={layout.navbarList}>
+            <img src={homeIcon} alt='img' className={layout.navbarListImg} />
+            <Link to="/" className={layout.navbarList}>Home</Link>
           </li>
-          <li className={layout.list}>Followers</li>
-          <li className={layout.list}>Following</li>
-          <li className={layout.list}>Public repo</li>
-          <li className={layout.list}>Created at</li>
+          <img src={folowersIcon} alt='img' className={layout.navbarListImg} />
+          <li className={layout.navbarList}>Followers</li>
+          <img src={followingIcon} alt='img' className={layout.navbarListImg} />
+          <li className={layout.navbarList}>Following</li>
+          <img src={repoIcon} alt='img' className={layout.navbarListImg} />
+          <li className={layout.navbarList}>Public-repo</li>
+          <img src={watchIcon} alt='img' className={layout.navbarListImg} />
+          <li className={layout.navbarList}>Created-at</li>
         </div>
       </ul>
     </div>
