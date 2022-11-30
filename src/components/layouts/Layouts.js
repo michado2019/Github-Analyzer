80b21f857db0cd85b51a3dc3c31aadb51aa0f254
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-12-01 00:56:43 +0100

    navbar done

--- src/components/layouts/Layouts.js
+++ src/components/layouts/Layouts.js
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import { Link } from "react-router-dom";
 import githubLogo from '../../assets/githubLogo.png'
 import layout from "./Layout.module.css";
@@ -5,15 +6,32 @@ import homeIcon from '../layouts/assets/home.png';
 import folowersIcon from '../layouts/assets/followers.png';
 import followingIcon from '../layouts/assets/following.png';
 import repoIcon from '../layouts/assets/repo.png';
-import watchIcon from '../layouts/assets/watch.png';
+
 //Navbar
 export const Navbar = () => {
+
+  // State
+  const [data, setData] = useState({});
+  // Fetch adata from gitbub api
+  async function getData() {
+    const url = 'https://api.github.com/users/michado2019';
+    const response = await fetch(url);
+    const results = await response.json();
+    setData({
+      name: results?.name,
+      avatar: results?.avatar_url,
+      followers: results?.followers,
+      following: results?.following,
+      repos: results?.public_repos,
+    });
+  }
+  getData()
   return (
     <div className={layout.navbarWrapper}>
       <ul className={layout.navbarListFlex}>
         <div className={layout.logoDiv}>
         <img src={githubLogo} alt="logo" className={layout.appLogo} />
-          <h2 className={layout.appLogoText}>Analazer App</h2>
+          <h2 className={layout.appLogoText}>Analazer</h2>
         </div>
         <div className={layout.navbarListsDiv}>
           <li className={layout.navbarList}>
@@ -21,13 +39,11 @@ export const Navbar = () => {
             <Link to="/" className={layout.navbarList}>Home</Link>
           </li>
           <img src={folowersIcon} alt='img' className={layout.navbarListImg} />
-          <li className={layout.navbarList}>Followers</li>
+          <a href='https://github.com/michado2019'><li className={layout.navbarList}>Followers <span className={layout.navbarFetchedData}>{data.followers}</span></li></a>
           <img src={followingIcon} alt='img' className={layout.navbarListImg} />
-          <li className={layout.navbarList}>Following</li>
+          <a href='https://github.com/michado2019'><li className={layout.navbarList}>Following <span className={layout.navbarFetchedData}>{data.following}</span></li></a>
           <img src={repoIcon} alt='img' className={layout.navbarListImg} />
-          <li className={layout.navbarList}>Public-repo</li>
-          <img src={watchIcon} alt='img' className={layout.navbarListImg} />
-          <li className={layout.navbarList}>Created-at</li>
+          <a href='https://github.com/michado2019'><li className={layout.navbarList}>Public-repo <span className={layout.navbarFetchedData}>{data.repos}</span></li></a>
         </div>
       </ul>
     </div>
